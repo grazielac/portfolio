44dba15e7768b3372a19ace01b87f3be525e6035
grazielac <cpgraziela@gmail.com>
2025-10-07 20:12:56 +0100

    Add nav links

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,17 @@
 export default function NavBar() {
     return (
-        <div>
+      <div className="flex justify-between bg-amber-100">
+        <div className="bg-amber-200">
             <h1>Welcome to my Portfolio</h1>
         </div>
-    )
+        <nav>
+            <a href="#" className="text-semibold">Home</a>
+            <a href="#" className="text-semibold">About</a>
+            <a href="#" className="text-semibold">Projects</a>
+            <a href="#" className="text-semibold">Contact</a>
+
+
+        </nav>
+      </div>
+    );
 }
