a3f4157e9f056ec8f913f47e71598cf7a20c3aec
grazielac <cpgraziela@gmail.com>
2025-10-07 20:28:51 +0100

    Add style to navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,38 @@
 export default function NavBar() {
-    return (
-      <div className="flex justify-between bg-amber-100">
-        <div className="bg-amber-200">
-            <h1>Welcome to my Portfolio</h1>
-        </div>
+  return (
+    <header
+      className="
+      top-0
+      flex justify-between
+      text-sm items-center 
+      p-4
+    bg-amber-100"
+    >
+      {/*left: logo */}
+      <div>
+        <h1 className="text-xl font-bold">Welcome to my Portfolio</h1>
+      </div>
+
+      {/*left: links */}
+      <div>
         <nav>
-            <a href="#" className="text-semibold">Home</a>
-            <a href="#" className="text-semibold">About</a>
-            <a href="#" className="text-semibold">Projects</a>
-            <a href="#" className="text-semibold">Contact</a>
-
-
+          <a href="#" className="hover:underline mr-4">
+            Home
+          </a>
+          <a href="#about" className="hover:underline mr-4">
+            About
+          </a>
+          <a href="#projects" className="hover:underline mr-4">
+            Projects
+          </a>
+          <a href="#contact" className="hover:underline mr-4">
+            Contact
+          </a>
+          <a href="./resume.pdf" target="blank" className="hover:underline mr-4">
+            Resume
+          </a>
         </nav>
       </div>
-    );
+    </header>
+  );
 }
