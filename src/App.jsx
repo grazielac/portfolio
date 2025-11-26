e8587c8684842e1ecfede149cfa0ee716237e853
grazielac <cpgraziela@gmail.com>
2025-11-26 12:46:48 +0000

    add favicon

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import Hero2 from "./components/Hero";
 import Projects from "./components/Projects";
 import About2 from "./components/About";
 import "./App.css";
-
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    document.title = "Graziela C | Software Developer";
+  }, []);
+
   return (
     <div>
       <Hero2 />
